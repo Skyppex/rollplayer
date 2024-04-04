43848d962ba40e5060bc8ef3cafb33d1fb984a92
Skypex <84551201+Skyppex@users.noreply.github.com>
2024-04-04 23:32:36 +0200

    fit hot reload

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Edit the App.tsx file to work on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
